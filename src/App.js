26c518af2f4684564129f337a98e680b336e6a44
ky213 <y.keddaoui@gmail.com>
2018-05-28 14:32:45 +0100

    Basic Math operations

--- src/App.js
+++ src/App.js
@@ -14,23 +14,53 @@ class App extends Component {
   }
 
   getInput(e) {
-    const input = e.target.innerHTML;
+    let input = e.target.innerHTML;
     let number = this.state.number;
     const activity = this.state.activity.slice()
+    const operation = this.state.operation;
 
-    if (input.match(/[\d\./\+-x]/)) {
+    if (input.match(/\d/)) {
       if (number.startsWith('0') && number.length > 0)
         number = number.slice(1)
 
       activity.push(input);
       this.setState({
-        number: number + input,
+        number: operation ? input : number + input,
         operation: false,
         activity: activity
       })
+      return;
     }
 
-    if(input.match)
+    if (input.match(/[+-/x]/)) {
+      activity.push(input)
+
+      this.setState({
+        operation: true,
+        activity: activity
+      })
+      return;
+    }
+
+    if (input.match(/=/)) {
+      let exp = activity.join('').replace(/x/g, "*")
+      try {
+        let result = eval(exp) + '';
+        if(result.length > 10)
+          result = result.slice(0,11)
+        this.setState({
+          number: result,
+          operation: true,
+        })
+      } catch (e) {
+        this.setState({
+          number: 'Error',
+          operation: true,
+        })
+      }
+      return
+    }
+
 
     if (input.match(/AC/))
       this.setState({
@@ -39,10 +69,10 @@ class App extends Component {
         activity: []
       })
 
-    if (input.match(/CE/)){
+    if (input.match(/CE/)) {
       const activity = this.state.activity.slice();
       const number = this.state.number;
-      const newNumber = number.slice(0, number.length-1);
+      const newNumber = number.slice(0, number.length - 1);
 
       activity.pop();
       this.setState({
@@ -50,8 +80,6 @@ class App extends Component {
         activity: activity
       })
     }
-     
-
   }
 
   render() {
